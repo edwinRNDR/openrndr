a143b50d5ce918565515c1f5feebc0f61cd5cd94
vechro <stenmr@gmail.com>
2021-04-17 12:40:29 +0300

    Enumerate valid StyleAttributes

---
+++ openrndr-svg/src/main/kotlin/org/openrndr/svg/StyleAttribute.kt
@@ -0,0 +1,146 @@
+package org.openrndr.svg
+
+enum class StyleAttribute {
+//    Amplitude,
+//    Azimuth,
+//    BaseFrequency,
+    BaselineShift,
+//    Bias,
+    Class,
+    ClipPath, // ?
+    ClipRule, // ?
+    ClipPathUnits,
+    Color,  // ?
+    ColorInterpolationFilters, // ?
+    Cx,
+    Cy,
+    D,
+//    DiffuseConstant,
+    Direction, // ?
+    Display, // ?
+//    Divisor,
+    Dx,
+    Dy,
+//    EdgeMode,
+//    Elevation,
+    EnableBackground, // ?
+//    Exponent,
+    Fill,
+    FillOpacity, // ?
+    FillRule, // ?
+//    Filter,
+//    FilterUnits,
+    FloodColor, // ?
+    FloodOpacity, // ?
+    FontFamily,
+    FontSize,
+    FontStretch,
+    FontStyle,
+    FontVariant,
+    FontWeight,
+    Fx,
+    Fy,
+    GradientTransform,
+    GradientUnits,
+    Height,
+    Href, // ???
+    Id,
+    ImageRendering, // ?
+//    In,
+//    In2,
+//    Intercept,
+//    K1,
+//    K2,
+//    K3,
+//    K4,
+//    KernelMatrix,
+//    KernelUnitLength,
+    LetterSpacing, // ?
+    LightingColor, // ?
+//    LimitingConeAngle,
+    MarkerEnd, // ?
+    MarkerMid, // ?
+    MarkerStart, // ?
+    MarkerHeight,
+    MarkerUnits,
+    MarkerWidth,
+    Mask, // ?
+    MaskContentUnits,
+    MaskUnits,
+//    Mode,
+//    NumOctaves,
+    Offset,
+    Opacity, // ?
+//    Operator,
+//    Order,
+    Orient,
+    Overflow, // ?
+    PathLength, // ???
+    PatternContentUnits,
+    PatternTransform,
+    PatternUnits,
+    Points,
+//    PointsAtX,
+//    PointsAtY,
+//    PointsAtZ,
+//    PreserveAlpha,
+    PreserveAspectRatio,
+//    PrimitiveUnits,
+    R,
+//    Radius,
+    RefX,
+    RefY,
+    RequiredExtensions,
+    RequiredFeatures,
+//    Result,
+    Rotate,
+    Rx,
+    Ry,
+//    Scale,
+//    Seed,
+    ShapeRendering, // ?
+//    Slope,
+    Space, // ?
+//    SpecularConstant,
+//    SpecularExponent,
+    SpreadMethod,
+    StartOffset,
+//    StdDeviation,
+//    StitchTiles,
+    StopColor, // ?
+    StopOpacity, // ?
+    Stroke, // ?
+    StrokeDasharray, // ?
+    StrokeDashoffset, // ?
+    StrokeLinecap, // ?
+    StrokeLinejoin, // ?
+    StrokeMiterlimit, // ?
+    StrokeOpacity, // ?
+    StrokeWidth, // ?
+    Style,
+//    SurfaceScale,
+    SystemLanguage,
+//    TableValues,
+//    TargetX,
+//    TargetY,
+    TextAnchor, // ?
+    TextDecoration, // ?
+    TextRendering, // ?
+    Transform,
+    Type,
+//    Values,
+    ViewBox,
+    Visibility, // ?
+    Width,
+    WordSpacing, // ?
+    WritingMode, // ?
+    X,
+    X1,
+    X2,
+//    XChannelSelector,
+    Y,
+    Y1,
+    Y2,
+//    YChannelSelector,
+//    Z
+}
